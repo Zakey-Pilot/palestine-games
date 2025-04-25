dc762bf14bf15456b388d9ec51b1a72102c1345a
Mohammed Naser Abu Shuqair <188046682+mo4shuqair@users.noreply.github.com>
2025-04-25 20:33:56 +0300

    integrate airtable with website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
-export default function Navbar(){
+export default async function Navbar(
+  {title}
+){
     return(
         <>
             <div className="bg-black p-4">
-                <h1 className="font-bold text-white">Palestine Games</h1>
+                <h1 className="font-bold text-white"> {title}</h1>
             </div>
         </>
     )
